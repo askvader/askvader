3c0580bc2a9bcbebb1ccf548d97b15de1688fcfc
Hans <hans@hanshoglund.se>
2019-01-14 12:28:57 +0000

    Misc

---
+++ release.nix
@@ -0,0 +1,65 @@
+
+let
+  opts = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          insert-ordered-containers = pkgs.haskell.lib.doJailbreak haskellPackagesOld.insert-ordered-containers;
+          these = pkgs.haskell.lib.doJailbreak haskellPackagesOld.these;
+          HaTeX = pkgs.haskell.lib.doJailbreak haskellPackagesOld.HaTeX;
+          wl-pprint-extras = pkgs.haskell.lib.doJailbreak haskellPackagesOld.wl-pprint-extras;
+        };
+      };
+    };
+  };
+
+  pkgs = import (builtins.fetchTarball {
+    url = https://github.com/nixos/nixpkgs/archive/6b80db3f5fae2e72f7a06943abe3cfae2791e8e8.tar.gz;
+    sha256 = "1909m8p20spf8779ilkwvszndcysf866bkhzzrxrhvxkmy08k3mf";
+  }) { config = opts; };
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "blog";
+  buildInputs = [
+    pkgs.nixops
+    pkgs.terraform
+    pkgs.packer
+    pkgs.nix
+    pkgs.fswatch
+    pkgs.gmp
+    pkgs.zlib
+    pkgs.dnsutils
+    pkgs.less
+    (
+    pkgs.haskellPackages.ghcWithPackages (hsPkgs: with hsPkgs;
+        [ aeson
+          base-prelude
+          parsec
+          parsers
+          boxes
+          dlist
+          mtl
+          pandoc
+          trifecta
+          parsers
+          data-fix
+          vinyl
+          extensible-effects
+          async
+          lens
+          text
+          neat-interpolation
+	  HaTeX
+          streams
+          aeson
+          warp-tls
+          wai-websockets
+          dhall-json
+          dhall-text
+        ])
+    )
+   ];
+  shellHook = ''
+  '';
+}
